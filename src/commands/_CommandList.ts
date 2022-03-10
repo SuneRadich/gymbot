69731f6143bef26737156de397e92ac6c4971d4e
Sune Radich Vestergaard <sune.radich.vestergaard@velliv.dk>
2022-03-10 22:31:04 +0100

    feat: Add invite link

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,6 +1,12 @@
 import { ICommand } from '../interfaces/Command';
 import { match } from './match';
 import { registerCompetition } from './register-competition';
+import { sendInvite } from './send-invite';
 import { latest } from './standings';
 
-export const CommandList: ICommand[] = [registerCompetition, match, latest];
+export const CommandList: ICommand[] = [
+  registerCompetition,
+  match,
+  latest,
+  sendInvite,
+];
