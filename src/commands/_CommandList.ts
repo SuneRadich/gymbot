9a527de99a74551a005fe3d8de821ba85e0fd3eb
Sune Radich Vestergaard <sune.radich.vestergaard@velliv.dk>
2022-04-10 10:54:55 +0200

    fix: Remove gspy command

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,13 +1,11 @@
 import { ICommand } from '../interfaces/Command';
 import { listCompetitions } from './list-competitions';
-import { match } from './match';
 import { registerCompetition } from './register-competition';
 import { sendInvite } from './send-invite';
 import { latest } from './standings';
 
 export const CommandList: ICommand[] = [
   registerCompetition,
-  // match,
   latest,
   sendInvite,
   listCompetitions,
